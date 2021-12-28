c3ae9818a1a3c6dcbf326cceacff9848f0e8337d
Jameson Hale <halejameson@gmail.com>
2021-12-28 11:27:50 -0500

    initial commit + exercise 1

---
+++ ex1/go.mod
@@ -0,0 +1,3 @@
+module gophercises/ex1
+
+go 1.17
---
+++ hello/hello.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"rsc.io/quote"
+//	"time"
+//	"math"
+)
+
+func main() {
+	fmt.Println("Hello, World!")
+	fmt.Println(quote.Go())
+	fmt.Println(quote.Glass())
+	fmt.Println(quote.Hello())
+	fmt.Println(quote.Opt())
+}
